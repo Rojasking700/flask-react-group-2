93c42eba38fca5958bcbf7979de742f4359b94cb
gabriel rojas <grojaspiano@gmail.com>
2021-03-21 16:45:26 -0500

    create account update-GR

--- src/views/CreateAcount.js
+++ src/views/CreateAcount.js
@@ -22,7 +22,11 @@ export default class CreateAcount extends Component {
             body: JSON.stringify({
                 "username": e.target.username.value,
                 "email": e.target.email.value,
-                "password": e.target.password.value
+                "password": e.target.password.value,
+                "confirm_password": e.target.confrimPassword.value,
+                "token": null,
+                "token_expiration": null
+
             })
         })
         // let newAccount = await res.json();
@@ -36,14 +40,19 @@ export default class CreateAcount extends Component {
         }
         return (
 
-            <div>
+            <div className="container">
                 <form onSubmit={(e) => this.createAccount(e)}>
+                    <label for="exampleInputPassword1" className="form-label">Username</label>
                     <input type="text" className="form-control" name="username" placeholder="Username" />
                     <br></br>
+                    <label for="exampleInputPassword1" className="form-label">Email</label>
                     <input type="text" className="form-control" name="email" placeholder="Email" />
                     <br></br>
-                    <input type="text" className="form-control" name="password" placeholder="Password" />
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="passwordField" className="form-control" name="password" placeholder="Password" />
                     <br></br>
+                    <label for="exampleInputPassword1" className="form-label">Confirm Password</label>
+                    <input type="passwordField" className="form-control" name="confrimPassword" placeholder="Confirm Password" />
                     <button type="submit" className="btn btn-outline-info">Submit</button>
                     <br></br>
                 </form>
